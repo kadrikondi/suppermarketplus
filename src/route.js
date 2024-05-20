ef9e7feff4b698617aaf44d43d0e27f37baf53ab
ayinde8901 <ayindebalogun@gmail.com>
2024-05-20 04:58:25 -0700

    resolve home

--- src/route.js
+++ src/route.js
@@ -39,38 +39,4 @@ export default function Approute() {
       </Switch>
     </>
   );
-<<<<<<< HEAD
 }
-
-import { Switch, Route } from "react-router-dom";
-import LandingPage from "./components/pages/LandingPage";
-import React from "react";
-import Signin from "./components/pages/Signin";
-import Signup from "./components/pages/Signup";
-import Home from "./components/pages/Home";
-
-export default function Approute() {
-  return (
-    <>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route exact path="/signin">
-          <Signin />
-        </Route>
-
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-
-        <Route exact path="/Home">
-          <Home />
-        </Route>
-      </Switch>
-    </>
-  );
-}
-=======
-}
->>>>>>> 3c6e086e57b2a1f219abc2b0e57645a7bd4e5560
