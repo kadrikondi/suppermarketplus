d9ad9a727a1bc4f494d0031ba961baf715a5e3ed
kadrikondi <abdulkadri42@gmail.com>
2024-05-20 11:52:55 +0100

    suppermarket project strcucture

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Approute from "./route";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route component={Header} />
+        <Route component={Approute} />
+        <Route component={Footer} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/route.js
@@ -0,0 +1,24 @@
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./components/pages/LandingPage";
+import React from "react";
+import Signin from "./components/pages/Signin";
+import Signup from "./components/pages/Signup";
+
+export default function Approute() {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route exact path="/signin">
+          <Signin />
+        </Route>
+
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </>
+  );
+}
