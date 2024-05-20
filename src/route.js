0da99abeb90cbf3acba85d516c293e7cdc3a68d2
omogbolahan355 <omogbolahan356@gmail.com>
2024-05-20 05:51:09 -0600

    contact

--- src/route.js
+++ src/route.js
@@ -3,6 +3,7 @@ import LandingPage from "./components/pages/LandingPage";
 import React from "react";
 import Signin from "./components/pages/Signin";
 import Signup from "./components/pages/Signup";
+import Contact from "./components/pages/contact";
 
 export default function Approute() {
   return (
@@ -18,6 +19,12 @@ export default function Approute() {
         <Route exact path="/signup">
           <Signup />
         </Route>
+
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+
+
       </Switch>
     </>
   );
