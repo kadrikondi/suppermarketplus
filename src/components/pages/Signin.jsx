a802622ecc10ed0dec689973c5aee25b0f74aa2b
kadrikondi <abdulkadri42@gmail.com>
2024-05-20 21:49:29 +0100

    update boostrap

--- src/components/pages/Signin.jsx
+++ src/components/pages/Signin.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { Button } from "react-bootstrap";
 export default function Signin() {
-  return <div>LOgin</div>;
+  return (
+    <div>
+      LOgin
+      <Button>Login</Button>
+    </div>
+  );
 }
--- src/route.js
+++ src/route.js
@@ -3,7 +3,7 @@ import LandingPage from "./components/pages/LandingPage";
 import React from "react";
 import Signin from "./components/pages/Signin";
 import Signup from "./components/pages/Signup";
-import Contact from "./components/pages/contact";
+import Contact from "./components/pages/Contact";
 import About from "./components/pages/About";
 import Userprofile from "./components/pages/Userprofile";
 
