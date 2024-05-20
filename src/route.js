f63aa8af732a30ec66de9e8eea6dd5da74e7d09a
omogbolahan355 <omogbolahan356@gmail.com>
2024-05-20 06:04:01 -0600

    add contact

--- src/route.js
+++ src/route.js
@@ -37,41 +37,10 @@ export default function Approute() {
          <Route exact path="/urserprofile/:id">
           <Userprofile />
         </Route>
-      </Switch>
-    </>
-  );
-<<<<<<< HEAD
-}
-
-import { Switch, Route } from "react-router-dom";
-import LandingPage from "./components/pages/LandingPage";
-import React from "react";
-import Signin from "./components/pages/Signin";
-import Signup from "./components/pages/Signup";
-import Home from "./components/pages/Home";
-
-export default function Approute() {
-  return (
-    <>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route exact path="/signin">
-          <Signin />
-        </Route>
-
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-
-        <Route exact path="/Home">
-          <Home />
+        <Route exact path="/contact">
+          <Contact />
         </Route>
       </Switch>
     </>
   );
 }
-=======
-}
->>>>>>> 3c6e086e57b2a1f219abc2b0e57645a7bd4e5560
