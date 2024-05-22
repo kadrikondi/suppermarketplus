9f964d08b0b278eb91194c757cab3ef4c55f0c1f
omogbolahan355 <omogbolahan356@gmail.com>
2024-05-22 06:36:43 -0600

    contact

--- src/route.js
+++ src/route.js
@@ -6,14 +6,8 @@ import Signup from "./components/pages/Signup";
 import Contact from "./components/pages/Contact";
 import About from "./components/pages/About";
 import Userprofile from "./components/pages/Userprofile";
-<<<<<<< HEAD
 import Storedetail from "./components/pages/Storedetail"
 import Createstore from "./components/pages/Createstore"
-=======
-import Storedetail from "./components/pages/Storedetail";
-import Createstore from "./components/pages/Createstore";
-
->>>>>>> c05a4ffa739eccca6cf8f0148f3c9e646ce1d7ab
 
 export default function route() {
   return (
