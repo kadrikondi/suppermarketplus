4f100b53cc1472ced15b439724f436d8923b52ff
kadrikondi <abdulkadri42@gmail.com>
2024-05-22 14:05:52 +0100

    sign in leave like dat

--- src/components/pages/Signin.jsx
+++ src/components/pages/Signin.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
 
 function Signin() {
   const [validated, setValidated] = useState(false);
@@ -19,54 +19,73 @@ function Signin() {
   };
 
   return (
-    <div style={{height: '250px', borderRadius: '50px', backgroundColor: '#ccc', width: '500px', margin: 'auto', color: 'black', alignItem: 'center'}}><br></br><div style={{fontSize: '30px', fontWeight: 'bolder', textAlign: 'center'}}><span style={{color: 'green'}}>L</ span><span style={{color: 'red'}}>O</ span><span style={{color: 'blue'}}>G</ span><span style={{color: 'yellow'}}>I</ span><span style={{color: 'black'}}>N</ span> </div>
- <Form>
-      <Form.Group style={{width: '500px', margin: 'auto'}} as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Label column sm="2">
-          Email:
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="email" placeholder="@" />
-        </Col>
-      </Form.Group>
-      
-      <Form.Group style={{width: '500px', margin: 'auto'}}as={Row} className="mb-3" controlId="formPlaintextPassword">
-        <Form.Label column sm="2">
-          Password:
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="password" placeholder="Password" />
-        </Col><br /><br />
-       
-     
+    <div
+      style={{
+        height: "250px",
+        borderRadius: "50px",
+        backgroundColor: "#ccc",
+        width: "500px",
+        margin: "auto",
+        color: "black",
+        alignItem: "center",
+      }}
+    >
+      <br></br>
+      <div
+        style={{ fontSize: "30px", fontWeight: "bolder", textAlign: "center" }}
+      >
+        <span style={{ color: "green" }}>L</span>
+        <span style={{ color: "red" }}>O</span>
+        <span style={{ color: "blue" }}>G</span>
+        <span style={{ color: "yellow" }}>I</span>
+        <span style={{ color: "black" }}>N</span>{" "}
+      </div>
+      <Form>
+        <Form.Group
+          style={{ width: "500px", margin: "auto" }}
+          as={Row}
+          className="mb-3"
+          controlId="formPlaintextEmail"
+        >
+          <Form.Label column sm="2">
+            Email:
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control type="email" placeholder="@" />
+          </Col>
+        </Form.Group>
 
-
-      <Button style={{marginTop: '15px', marginLeft: '140px', color: 'White', borderRadius: '50px', backgroundColor: 'Skyblue', width: '230px'}} type="submit">Submit</Button>{' '}
-      </Form.Group>
-     
-
-    </Form>
-
-  
-   
-
+        <Form.Group
+          style={{ width: "500px", margin: "auto" }}
+          as={Row}
+          className="mb-3"
+          controlId="formPlaintextPassword"
+        >
+          <Form.Label column sm="2">
+            Password:
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+          <br />
+          <br />
+          <Button
+            style={{
+              marginTop: "15px",
+              marginLeft: "140px",
+              color: "White",
+              borderRadius: "50px",
+              backgroundColor: "Skyblue",
+              width: "230px",
+            }}
+            type="submit"
+          >
+            Submit
+          </Button>{" "}
+        </Form.Group>
+      </Form>
     </div>
   );
 }
 
 export default Signin;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
